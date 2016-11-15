5ced065eda7a71b66d49041bfe137358e43ae682
Richard Friedman <richardfriedman@yahoo.com>
2016-11-15 08:32:33 -0500

    initial commit with custom test, echo collection, and package.json

---
+++ CustomTest.js
@@ -0,0 +1,105 @@
+var fs = require('fs');
+var newman = require('newman');
+
+/**
+ * Required to accept parameters from test harness.
+ * @param redlineApi handle to API object
+ * @param testNum # number of test user
+ * @param rand identifier for test
+ * @param config config object loaded from loadtest.ini
+ */
+function PostmanTest(redlineApi, testNum, rand, config)
+{
+	// We need Redline API to report results, errors, requests.
+	this.redlineApi = redlineApi;
+
+	// Keep track of test information.
+	this.testNum = testNum;
+	this.rand = rand;
+
+	// This is the test configuration file (loadtest.ini)
+	this.config = config;
+
+	// Specify postman/newman options.
+	this.newmanCollection = './Echo.postman_collection';
+	this.newmanEnvironment = null;
+	this.newmanGlobals = null;
+	this.newmanIterations = 10;
+}
+
+/** 
+ * Run test and invoke callback on success/failure
+ */
+PostmanTest.prototype.runTest = function( redlineCallback )
+{
+	var that = this;
+	var items = {};
+
+	// Make request after timeout
+	setTimeout(function() {
+
+		newman.run({
+			collection: this.newmanCollection,
+			environment: this.newmanEnvironment,
+			globals: this.newmanGlobals,
+			iterationCount: this.newmanIterations,
+			reporters: ['cli','html'],
+			reporter: {html:{export:'output/report.html'}},
+			noColor: true
+		},function(error, summary){
+			console.log( error );
+			redlineCallback(false);
+		})
+		.on( 'beforeItem', function( err, args ){
+			items[args.cursor.ref] = {};
+		})
+		.on( 'beforeRequest', function( err, args ){
+			items[args.cursor.ref].start = Date.now();
+		})
+		.on( 'request', function( err, args ){
+			items[args.cursor.ref].response = args.response;
+			items[args.cursor.ref].end = Date.now();
+		})
+		.on( 'assertion', function( err, args ){
+			if ( err ) {
+				if ( !items[args.cursor.ref].errors ){
+					items[args.cursor.ref].errors = [];
+				} 
+				items[args.cursor.ref].errors.push( err );
+			}
+		})
+		.on( 'item', function( err, args ){
+			var name = '';
+			var url = '';
+			name = args.item.name;
+			if (args.item.request && args.item.request.url){
+				url = args.item.request.url;
+			}
+			
+			var isError = false;
+			var time = items[args.cursor.ref].end - items[args.cursor.ref].start;
+			var resTime =  items[args.cursor.ref].response.responseTime;
+			
+			var resCode = items[args.cursor.ref].response.code;
+			var resStatus = items[args.cursor.ref].response.status;
+			if ( items[args.cursor.ref].errors ){
+				isError = true;
+				var resError = '';
+				items[args.cursor.ref].errors.forEach(function(err,index){
+					that.redlineApi.recordError(err.message, items[args.cursor.ref].end / 1000);
+				})
+			}
+			var kb = items[args.cursor.ref].response.responseSize || 0;
+			
+			that.redlineApi.recordURLPageLoad(name, items[args.cursor.ref].end / 1000, resTime, isError, kb);
+			delete items[args.cursor.ref];
+		})
+		.on( 'exception', function( err, args ){
+			that.redlineApi.recordError("Exception in Newman " + args.cursor, Date.now() / 1000 );
+			redlineCallback(true);
+		})
+	}, 1 );
+};
+
+// Required Export for Test to be loaded and executed.
+module.exports = PostmanTest;
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# plugin-postman
+# Postman - RedLine13 Integration
+
+You can run any Postman collection on RedLine13 as a performance test with this custom test type.  This can be tested locally using the test harness (https://github.com/redline13/harness-custom-test-nodejs) or directly on RedLine13 at scale.
+
+### Step 1 Modify test for your collection and data.
+- Modify CustomTest.js in the constuctor to override the defaults. These options are the API options defined in newman (https://github.com/postmanlabs/newman#api-reference)
+	- this.newmanCollection = './Echo.postman_collection';
+		- Specify the collection file that you will be attaching to the test. The file will be in the same directory which test is run from, use ./COLLECTION
+	- this.newmanEnvironment = null;
+		- Specify environment file (or url to environment file)
+	- this.newmanGlobals = null;
+		- Postman Global Variables can be optionally passed on to a collection run in form of path to a file or URL.
+	- this.newmanIterations = 1;
+		- Specify the number of iterations to run on the collection. 
+
+### Step 2 Package your test
+- We require the package.json which is provided and includes newman.  For this reason we need to package our test run. 
+
+	```tar cvf postman.tar CustomTest.js Echo.postman_collection.json package.json```
+- Include your test(CustomTest.js), collection(Postman collection), package.json
+
+### Step 3 Run your test.
+- Login to https://www.redline13.com/Service 
+	- RedLine13 enables load tests on your cloud, you need to first setup AWS Keys.
+	- This test requires the NVM plugin, add to your account @ https://www.redline13.com/Account/plugins
+- Start test
+- Select Custom test
+- Click Upload Custom and select the tar we packaged earlier
+- Select # of users to simulate 
+- Required: Advanced Custom Test Options
+	- Select plugin "Node Version Manager"
+		- Select 6.* as the node version
+		- Note: the Load Agent Language should have changed to Node.js
+- Optional: Modify Advanced Cloud Options to control # and location of servers.
+- Optional: Name your test.
+- Start test
+
+Questions, Feedback, Requests - info@redline13.com
